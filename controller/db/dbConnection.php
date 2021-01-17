f0a00baf3a90744e2f7fe0c8f96acae6fc6889c0
YoutubeMate <projectyt2146@gmail.com>
2021-01-17 11:38:59 +0530

    create branch for yohombu

--- controller/db/dbConnection.php
+++ controller/db/dbConnection.php
@@ -1,12 +1,24 @@
 <?php
     class DBConnection{
         private $connection;
-        private $dbConnetion = DBConnection;
+        private  $dbConnetion;
 
         private function DBConnection(){
-            $connection = mys
+            $connection = new mysqli_connect("localhost","root","mysql","thogakade");
+        }
+        public function getInstance(){
+            if($connection==NULL){
+                return new DBConnection();
+            }else{
+                return $dbConnetion;
+            }
+        }
+        public function getConnection(){
+            return $connection;
         }
 
+
     }
+    (new DBConnection())::getConnection()->getInstance();
     
 ?>
